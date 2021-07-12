a0aecdad663fced51ca3125a4e9ab9c15dd9bfe7
José Israel Santizo Santos <joseisraelss.JISS@gmail.com>
2021-07-11 22:28:11 -0600

    Se coloca incono para theme

---
+++ index.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&display=swap" rel="stylesheet"
+    />
+    <link rel="shortcut icon" href="img/car-sales.png" type="image/x-icon">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="https://kit.fontawesome.com/1085598771.js" crossorigin="anonymous"></script>
+    <title>Car Sale</title>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="container">
+            <div class="container-navbar ">
+                <div class="logo-navbar">
+                    <img src="https://intecap.edu.gt/wp-content/uploads/2020/03/logo-intecap.png" alt="Logo">
+                </div>
+                <div class="navbar">
+                    <ul>
+                        <li><a href="#"><i class="fas fa-sign-in-alt"></i></a></li>
+                        <li><a href="#"></a></li>
+                        <li><a href="#"></a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="theme-toggle">
+                <input type="checkbox" id="theme-toggle-btn">
+                <label for="theme-toggle-btn">
+                    <i class="fas fa-moon toggle-icon" aria-hidden="true"></i>
+                </label>
+            </div>
+            <div class="container-banner section-margin">
+                <h1>El auto que buscas lo tenemos</h1>
+            </div>
+            <div class="container-sale section">
+                <div class="item item-1">
+                    <div class="card card1-image">
+                        <img src="img/car4.jpg" alt="Car">
+                    </div>
+                    <div class="card1-description">
+                        <h2>Camaro 2017</h2>
+                        <span>Mecanico</span>
+                    </div>
+                </div>
+                <div class="item item-2">
+                    <div class="card card2-image">
+                        <img src="img/car2.jpg" alt="Car">
+                    </div>
+                    <div class="card1-description">
+                        <h2>Camaro 2017</h2>
+                        <span>Mecanico</span>
+                    </div>
+                </div>
+                <div class="item item-3">
+                    <div class="card card3-image">
+                        <img src="img/car3.jpg" alt="Car">
+                    </div>
+                    <div class="card3-description">
+                        <h2>Camaro 2017</h2>
+                        <span>Mecanico</span>
+                    </div>
+                </div>
+                <div class="item item-1">
+                    <div class="card card1-image">
+                        <img src="img/car4.jpg" alt="Car">
+                    </div>
+                    <div class="card1-description">
+                        <h2>Camaro 2017</h2>
+                        <span>Mecanico</span>
+                    </div>
+                </div>
+                <div class="item item-2">
+                    <div class="card card2-image">
+                        <img src="img/car2.jpg" alt="Car">
+                    </div>
+                    <div class="card1-description">
+                        <h2>Camaro 2017</h2>
+                        <span>Mecanico</span>
+                    </div>
+                </div>
+                <div class="item item-3">
+                    <div class="card card3-image">
+                        <img src="img/car3.jpg" alt="Car">
+                    </div>
+                    <div class="card3-description">
+                        <h2>Camaro 2017</h2>
+                        <span>Mecanico</span>
+                    </div>
+                </div>
+            </div>
+            <div class="container-footer section-margin">
+            <div class="logo-footer">
+                    <a href="https://intecap.edu.gt/" target="_blank"><img src="https://intecap.edu.gt/wp-content/uploads/2019/01/logo-inverse.png" alt="Logo"></a>
+                </div>
+                <div class="footer">
+                    <ul>
+                        <li><a href="https://github.com/IsraelSantizo98"><i class="fab fa-github"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="js/main.js"></script>
+</body>
+</html>
