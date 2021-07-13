78796273a1be771408a6102fcd74d08ddc385785
José Israel Santizo Santos <joseisraelss.JISS@gmail.com>
2021-07-12 21:35:49 -0600

    Se termina validacion de inicio de sesion

---
+++ login.php
@@ -0,0 +1,51 @@
+<?php
+    //evalua si existe un metodo postal
+    if ($_SERVER["REQUEST_METHOD"]== "POST") {
+        $userName = $_POST['userName'];
+        $pass = $_POST['pass'];//la ultima parte de toma del name del input
+        include 'php/conection.php';
+        $instruccion = "SELECT * FROM usuario WHERE userName = '$userName' AND passwordN = '$pass'";
+        $query =mysqli_query($conection, $instruccion);
+        //Se carga a $r el resulado 
+        $r = mysqli_fetch_array($query, MYSQLI_ASSOC);
+        //Se crea un contador de cuantos registros hay en $query y debe ser uno en el caso de usuario
+        $contador = mysqli_num_rows($query);
+        //Sei la variabale contador es 1 entonces se inicia una session
+        if($contador == 1){
+            //Crea un espacio en el servidor para que se puede acceder desde la computadora que se inicia seccion
+            session_start();
+            //variables de tipo session se puede acceder en cualquier ficheros, se coloca un name y luego el name que esta en la variable $r
+            $_SESSION['name'] = $r['name'];
+            //Se obtiene el resultado del metodo post
+            $_SESSION['userName'] = $userName;
+            //$_SESSION['usuario'] = $r['usuario'];
+            //con esta instruccion se envia a otro archivo
+            header('location: main.php');
+        }
+    }
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Login</title>
+</head>
+<body>
+    <div class="container-login">
+        <form method="POST">
+            <fieldset>
+                <legend>Inicio de Sesion</legend>
+                <label for="userName">Ingrese nombre de usuario</label>
+                <input type="text" id="userName" name="userName" placeholder="Nombre de usuario">
+                <label for="pass">Ingrese su contraseña</label>
+                <input type="password" name="pass" id="pass" placeholder="Contraseña">
+                <input type="hidden" name="entrar" value="entrar">
+                <input class="btn btn-submit" type="submit" value="Inciar Sesion">
+            </fieldset>
+        </form>
+    </div>
+</body>
+</html>
