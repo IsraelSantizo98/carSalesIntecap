ce9b291a37c0ef815c0d65229fa2955f407fbd09
José Israel Santizo Santos <joseisraelss.JISS@gmail.com>
2021-07-18 17:44:57 -0600

    Eliminacion de codigo para validar password

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/*Theme */
 const themeToggleBtn = document.querySelector("#theme-toggle-btn");
 const toggleIcon = document.querySelector(".toggle-icon");
 themeToggleBtn.addEventListener("change", function () {
@@ -23,88 +24,59 @@ if (localStorage.getItem("dark-mode") === "true") {
     toggleIcon.classList.remove("fa-sun");
     toggleIcon.classList.add("fa-moon");
 }
-/* login validation */
-let state = false;
-let password = document.getElementById("password");
-let passwordStrength = document.getElementById("password-strength");
-let lowUpperCase = document.querySelector(".low-upper-case i");
-let number = document.querySelector(".one-number i");
-let specialChar = document.querySelector(".one-special-char i");
-let eightChar = document.querySelector(".eight-character i");
-
-password.addEventListener("keyup", function () {
-let pass = document.getElementById("password").value;
-    checkStrength(pass);
-});
-//Muestra la contraseña, en el if state es true o false es un valor booleano
-function toggle() {
-    if (state) {
-        document.getElementById("password").setAttribute("type", "password");
-        state = false;
-    } else {
-        document.getElementById("password").setAttribute("type", "text");
-        state = true;
+/* Carousel */
+//Guarda cada una de las fotos que esten con ese nombre de clase y se transforma en un array
+//array permite almacenar varios valores y acceder a cada objeto e inicia con el indice 0.
+var loteFotos = document.getElementsByClassName('fotoCarousel');
+//Se agregan contador para poder tener registro de que foto colocaremos el disploy block
+var contador = 0;
+//Se agrega con el indice inicial con display block para que se ve la primera imagen
+loteFotos[0].style.display='block';
+var array = loteFotos;
+console.log(array)
+const getLastArrItem = (arr) => {  
+    let lastItem = arr.pop();
+    console.log(`Last element is ${lastItem}`);
+} 
+getLastArrItem(array);
+//la funcion es para ir a la siguiente imagen
+function nextImage(){
+    //Se suma al contador para que un valor aumentado por 1 en cada click de boton
+    contador++;
+    //Condiciona a que cuando contador sea el total de la capacidad de length entonces regrese a 0
+    if(contador == loteFotos.length){
+        contador=0;
     }
+    //Este ciclo sirve para colocar todo a display none, trabaja en conjunto con la variable contador para que contador se vaya aumentado pero el for pone todo en none
+    for(var i = loteFotos.length -1; i>=0; i--){ //Coloca la variable i como el ultimo elemento del array, coloca i que sea mayor o igual a 0 y entonces le resta 1 en otras palabras se inicializa con el numero mayor del array en este caso 5 y mientras i sea mayor o igual a 0 le restara 1 cada vez
+        loteFotos[i].style.display='none';//Coloca todas la fotos en 0
+        //console.log("Prueba"+loteFotos[i])
+        //document.getElementsByClassName('fotoCarousel').style.display='block'
+    }
+    console.log(contador);
+    //Luego ya que contador se sumo y que el for puso las demas fotos con none, este se coloca el valor sumado de contador para hacer que solo una foto tenga el display block
+    loteFotos[contador].style.display='block';
+};
+function previousImage(){
+    contador--;
+    //Condiciona a que cuando el contador sea -1 (un elemento que no existe en el array) entonces el contador pasara el maximo de incide del array
+    if(contador == -1){
+        let lastItem = loteFotos.pop();
+        console.log(lastItem)
+        //contador = 4;
+        //contador=loteFotos[loteFotos.length+1];
+    }
+    for(var i = loteFotos.length -1; i>=0; i--){
+        loteFotos[i].style.display='none';
+        //console.log("Prueba"+loteFotos[i])
+        //document.getElementsByClassName('fotoCarousel').style.display='block'
+    }
+    loteFotos[contador].style.display='block';
+};
+/*
+for(var i = 0; i < loteFotos.length; i++){
+    loteFotos[i].style.display='block';
+    console.log("Prueba"+loteFotos[i])
+    //document.getElementsByClassName('fotoCarousel').style.display='block'
 }
-//Muestra la contraseña al presionar el icono al igual que la funcion de arriba
-function myFunction(show) {
-    show.classList.toggle("fa-eye-slash");
-}
-//Funcion para validar que tenga caracteres especiales, numeros y letras
-function checkStrength(password) {
-    let strength = 0;
-
-  //If password contains both lower and uppercase characters
-    if (password.match(/([a-z].*[A-Z])|([A-Z].*[a-z])/)) {
-        strength += 1;
-        lowUpperCase.classList.remove("fa-circle");
-        lowUpperCase.classList.add("fa-check");
-    } else {
-        lowUpperCase.classList.add("fa-circle");
-        lowUpperCase.classList.remove("fa-check");
-    }
-  //If it has numbers and characters
-    if (password.match(/([0-9])/)) {
-        strength += 1;
-        number.classList.remove("fa-circle");
-        number.classList.add("fa-check");
-    } else {
-        number.classList.add("fa-circle");
-        number.classList.remove("fa-check");
-    }
-  //If it has one special character
-    if (password.match(/([!,%,&,@,#,$,^,*,?,_,~])/)) {
-        strength += 1;
-        specialChar.classList.remove("fa-circle");
-        specialChar.classList.add("fa-check");
-    } else {
-        specialChar.classList.add("fa-circle");
-        specialChar.classList.remove("fa-check");
-    }
-  //If password is greater than 7
-    if (password.length > 7) {
-        strength += 1;
-        eightChar.classList.remove("fa-circle");
-        eightChar.classList.add("fa-check");
-    } else {
-        eightChar.classList.add("fa-circle");
-        eightChar.classList.remove("fa-check");
-    }
-  // If value is less than 2
-    if (strength < 2) {
-        passwordStrength.classList.remove("progress-bar-warning");
-        passwordStrength.classList.remove("progress-bar-success");
-        passwordStrength.classList.add("progress-bar-danger");
-        passwordStrength.style = "width: 10%";
-    } else if (strength == 3) {
-        passwordStrength.classList.remove("progress-bar-success");
-        passwordStrength.classList.remove("progress-bar-danger");
-        passwordStrength.classList.add("progress-bar-warning");
-        passwordStrength.style = "width: 60%";
-    } else if (strength == 4) {
-        passwordStrength.classList.remove("progress-bar-warning");
-        passwordStrength.classList.remove("progress-bar-danger");
-        passwordStrength.classList.add("progress-bar-success");
-        passwordStrength.style = "width: 100%";
-    }
-}
+*/
